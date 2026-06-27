93416042ed1b7b4509aa3ed44b11cc675d6b8051
Pouya Asgharnejad Tehran <pouyaaofficial@gmail.com>
2026-06-27 19:39:15 +0330

    ref: make ai reusable package

--- app/Assist/Commands/McpServerCommand.php
+++ app/Assist/Commands/McpServerCommand.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace App\Assist\Commands;
 
-use App\AI\Mcp\McpServer;
 use Mcp\Server\Transport\StdioTransport;
 use Psr\Log\LoggerInterface;
+use Sakoo\AI\Mcp\Dto\McpServerDto;
+use Sakoo\AI\Mcp\McpServer;
 use Sakoo\Framework\Core\Console\Command;
 use Sakoo\Framework\Core\Console\Input;
 use Sakoo\Framework\Core\Console\Output;
+use System\AI\ProjectContext;
+use System\Path\Path;
 
 /**
  * You can test command using below command:
@@ -36,8 +39,20 @@ class McpServerCommand extends Command
 			 * @phpstan-ignore argument.type
 			 */
 			$logger = resolve('logger.ai');
+			$context = resolve(ProjectContext::class);
 
-			McpServer::factory()->run(new StdioTransport(logger: $logger));
+			$dto = new McpServerDto(
+				path: Path::getAppDir() . '/AI/Mcp',
+				name: 'Sakoo PHP Framework MCP Server',
+				version: '1.0.0',
+				description: 'This MCP Server provides all of necessary Components to communicate with LLMs.',
+				websiteUrl: 'https://sakoo.dev',
+				icons: 'https://avatars.githubusercontent.com/u/86832974?s=200&v=4',
+				sessionPath: $context->storageDir() . '/ai/mcp-sessions',
+				logger: $logger,
+			);
+
+			McpServer::factory($dto)->run(new StdioTransport(logger: $logger));
 		} catch (\Throwable $e) {
 			fwrite(STDERR, '[CRITICAL ERROR] ' . $e->getMessage() . "\n");
 
